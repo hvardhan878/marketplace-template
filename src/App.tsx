03affcd25a02c28287cd49e4988a2a27f36ca611
hvardhan878 <hvardhan878@gmail.com>
2025-01-14 17:08:27 +0000

    Removed category bar

--- src/App.tsx
+++ src/App.tsx
@@ -52,7 +52,6 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-50">
       <Navbar />
-      <CategoryBar />
       
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <section className="mb-12">
